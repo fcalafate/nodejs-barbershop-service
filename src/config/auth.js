773fa9491db5d031e4caaf7b29e2470848514a46
Felipe Calafate <flp.calafate@gmail.com>
2019-07-09 17:03:59 -0300

    Initial commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'fuwe98fw98fu8wfe899w8e9w8f89wefs2312e09erf',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import AuthMiddleware from './app/middlewares/auth';
+import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionController';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionControler.store);
+
+routes.use(AuthMiddleware);
+
+routes.put('/users', UserController.update);
+
+export default routes;
